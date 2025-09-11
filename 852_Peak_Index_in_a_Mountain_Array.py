998d69bc5935553fd5b9363f482e013d7e576b35
Allen Huang <allen2990167@gmail.com>
2025-09-10 23:14:09 -0700

    2 - Add LC problem

---
+++ 852_Peak_Index_in_a_Mountain_Array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: list[int]) -> int:
+        if not arr:
+            return -1
+
+        start, end = 0, len(arr) - 1
+        
+        while start + 1 < end:
+            mid = (start + end) // 2
+            
+            if arr[mid] > arr[mid + 1]:
+                end = mid
+            
+            else:
+                start = mid
+        
+        return end
+
+if __name__ == '__main__':
+    sol = Solution()
+    result = sol.peakIndexInMountainArray([0,1,0])
+    print(result)
+    result = sol.peakIndexInMountainArray([0,2,1,0])
+    print(result)
+    result = sol.peakIndexInMountainArray([0,10,5,2])
+    print(result)
